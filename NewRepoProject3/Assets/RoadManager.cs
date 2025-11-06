e610b53a80365cf6f0db2bee480b20dcbd140778
greatbrick <speharley@gmail.com>
2025-11-06 17:49:39 -0500

    Added Object Pooling For Roads

--- NewRepoProject3/Assets/RoadManager.cs
+++ NewRepoProject3/Assets/RoadManager.cs
@@ -10,6 +10,7 @@ public class RoadManager : MonoBehaviour
     private GameObject roadObj;
     private List<GameObject> roadRefs = new List<GameObject>();
     private float currentDistance = 0;
+    private Stack<GameObject> roadPool = new Stack<GameObject>();
 
     private void Start()
     {
@@ -22,16 +23,38 @@ public class RoadManager : MonoBehaviour
 
     private void AddNewRoad()
     {
-        roadRefs.Add(Instantiate(roadObj, transform));
+        roadRefs.Add(GetNewRoad());
         roadRefs[roadRefs.Count - 1].transform.position = Vector2.up * currentDistance;
         currentDistance += 10;
         playerManager.milestoneDistance += 10;
     }
 
+    private GameObject GetNewRoad()
+    {
+        GameObject output;
+        if (roadPool.Count > 0)
+        {
+            output = roadPool.Pop();
+            output.SetActive(true);
+        }
+        else
+        {
+            output = Instantiate(roadObj, transform);
+            Debug.Log("Instantiation case");
+        }
+        return output;
+    }
+
+    private void DeactivateRoad(GameObject rr)
+    {
+        rr.SetActive(false);
+        roadPool.Push(rr);
+    }
+
     private void CycleRoads()
     {
         AddNewRoad();
-        Destroy(roadRefs[0]);
+        DeactivateRoad(roadRefs[0]);
         roadRefs.RemoveAt(0);
     }
 }
